04e1ca1c375cf9f4e69698ae642f83b86dd38ad4
yaoli <liyao.zheda@gmail.com>
2018-07-06 13:38:02 +0800

    add pair example

---
+++ Pair Example/pairServer.py
@@ -0,0 +1,18 @@
+import zmq
+import random
+import sys
+import time
+
+port = "5556"
+context = zmq.Context()
+socket = context.socket(zmq.PAIR)
+socket.bind("tcp://*:%s" % port)
+
+while True:
+   socket.send("Server message to client3")
+
+   print socket.recv()
+#   print socket.recv()
+   print time.time()
+
+   time.sleep(1)
